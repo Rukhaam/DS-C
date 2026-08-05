ee3cf169f37c6fd8d3c5fae0af6845d311094d1e
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-08-05 12:59:55 +0530

    strings floder created with frist prog of concat

---
+++ strings/concatination.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void concatstr(char str1[],char str2[]){
+
+    int i=0;
+    int j =0;
+
+    while(str1[i] != '\0'){
+        i++;
+    }
+      while(str2[j]!='\0'){
+        str1[i]=str2[j];
+        i++;
+        j++;
+    }
+    
+    str1[i] = '\0';
+
+    printf("the concatenated string is %s",str1);
+}
+
+int main(){
+    char str1[100] = "HELLO";
+    char str2[100] = "WORLD";
+
+    concatstr(str1,str2);
+    return 0;
+
+
+}
