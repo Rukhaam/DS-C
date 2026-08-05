c126661c87e891015d44c93701fed30f8c94c178
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-08-05 13:15:14 +0530

    Add reverse string functionality with associated object and debug files - Introduced a new implementation for reversing strings in the `reversestring.c` file. - Added compiled object file `reversestring.obj` and intermediate link file `reversestring.ilk`. - Included program database file `reversestring.pdb` for debugging purposes.

---
+++ strings/reversestring.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void reverseString(char str[]) {
+    int len = 0;
+    char revStr[100];
+
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    for (int i = 0; i < len; i++) {
+        revStr[i] = str[len - 1 - i];
+    }
+    revStr[len] = '\0';
+
+    printf("%s\n", revStr);
+}
+
+int main() {
+    char str[100] = "rukhaam";
+    reverseString(str);
+    return 0;
+};
