a838819ff700efb8b38316c1aff0c9edfd8142ab
rukhaam19 <rukhaammushtaq19@gmail.com>
2026-09-04 21:30:20 +0530

    Update second string in concatenation example from "pendu" to "RUKHAAM"

--- strings/concatination.c
+++ strings/concatination.c
@@ -21,7 +21,7 @@ void concatstr(char str1[],char str2[]){
 
 int main(){
     char str1[100] = "HELLO";
-    char str2[100] = "pendu";
+    char str2[100] = "RUKHAAM";
 
     concatstr(str1,str2);
     return 0;
